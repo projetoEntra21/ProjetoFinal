b46ade80652b0013b540244babf09c0e554e4f69
juliamohr <87392720+juliamohr@users.noreply.github.com>
2021-09-10 17:25:29 -0300

    mudança nos index e adição de javascript nas telas de cadastro

---
+++ view/medidas/script.js
@@ -0,0 +1,15 @@
+function peso(peso){
+    if(peso >= 0 && peso <= 1000) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function altura(altura){
+    if(altura => 0 && altura <= 3.0) {
+        return true;
+    }else{
+        return false;
+    }
+}
