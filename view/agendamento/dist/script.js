1b78facf30ccf7c0d56985e047c76c1e7129f653
juliamohr <juliamcachoeira@gmail.com>
2021-09-20 10:43:17 -0300

    javascript para validação dos modais

--- view/agendamento/dist/script.js
+++ view/agendamento/dist/script.js
@@ -1,46 +1,39 @@
-function validadata(d){
+function validardata(d){
+   var data = d.value; // pega o valor do input
+   data = data.replace(/\//g, "-"); // substitui eventuais barras (ex. IE) "/" por hífen "-"
+   var data_array = data.split("-"); // quebra a data em array
+   var dia = data_array[2];
+   var mes = data_array[1];
+   var ano = data_array[0];
 
-    var data = d.value; // pega o valor do input
-    //rever
-   // data = data.replace(/\//g, "-"); // substitui eventuais barras (ex. IE) "/" por hífen "-"
-   // var data_array = data.split("-"); // quebra a data em array
-    var dia = data_array[2];
-    var mes = data_array[1];
-    var ano = data_array[0];
- 
-    // para o IE onde será inserido no formato dd/MM/yyyy
-    if(data_array[0].length != 4){
+   // para o IE onde será inserido no formato dd/MM/yyyy
+   if(data_array[0].length != 4){
+      dia = data_array[0];
+      mes = data_array[1];
+      ano = data_array[2];
+   }
 
-       dia = data_array[0];
-       mes = data_array[1];
-       ano = data_array[2];
-    }
- 
-    var hoje = new Date();
-    var d1 = hoje.getDate();
-    var m1 = hoje.getMonth()+1;
-    var a1 = hoje.getFullYear();
- 
-    var d1 = new Date(a1, m1, d1);
-    var d2 = new Date(ano, mes, dia);
- 
-    var diff = d2.getTime() - d1.getTime();
-    diff = diff / (1000 * 60 * 60 * 24);
-    
-    if(diff < 0){
+   var hoje = new Date();
+   var d1 = hoje.getDate();
+   var m1 = hoje.getMonth()+1;
+   var a1 = hoje.getFullYear();
 
-       console.log("A Data não pode ser anterior ao dia de hoje!");
-    }else if(diff > 30){
-       console.log("A Data não pode exceder o limite de 30 dias!");
-    }else{
-       console.log("Data válida!");
+   var d1 = new Date(a1, m1, d1);
+   var d2 = new Date(ano, mes, dia);
 
+   var diff = d2.getTime() - d1.getTime();
+   diff = diff / (1000 * 60 * 60 * 24);
+   
+   if(diff < 0){
       alert("Data não pode ser anterior ao dia de hoje!");
-    } if(diff > 30){
-      alert("Data não exceder o limite de 30 dias!");
+   }else if(diff > 30){
+      alert("Data não pode ser mais do que 30 dias pra frente!");
+   }else{
+      console.log("Data válida!");
+   }
+   
+}
 
-    }
-  }
   $('#meuModal').on('shown.bs.modal', function () {
     $('#meuInput').trigger('focus')
   })
