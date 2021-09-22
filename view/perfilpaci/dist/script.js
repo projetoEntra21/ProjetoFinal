4af93d7f2a4c783e20ac3619e7a8dc7fad03cb65
juliamohr <87392720+juliamohr@users.noreply.github.com>
2021-09-22 17:19:16 -0300

    javascript

--- view/perfilpaci/dist/script.js
+++ view/perfilpaci/dist/script.js
@@ -1,16 +1,20 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#meuInput').trigger('focus')
 })
-var password = document.getElementById("password"),
- confirm_password = document.getElementById("confirm_password");
 
-function validatePassword(){
-if(password.value != confirm_password.value) {
+var myPassword = document.getElementById("myPassword")
+, confirm_password = document.getElementById("confirm_password");
+
+function validatemyPassword(){
+if(myPassword.value != confirm_password.value) {
   confirm_password.setCustomValidity("Senhas diferentes!");
 } else {
   confirm_password.setCustomValidity('');
 }
 }
+
+myPassword.onchange = validatemyPassword;
+confirm_password.onkeyup = validatemyPassword;
 
   password.onchange = validatePassword;  
  confirm_password.onkeyup = validatePassword;
