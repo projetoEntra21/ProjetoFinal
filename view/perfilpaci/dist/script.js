ac2a909f517828d1a11ce62900690577824f23b7
juliamohr <87392720+juliamohr@users.noreply.github.com>
2021-09-24 17:12:55 -0300

    javascript na tela do paciente

--- view/perfilpaci/dist/script.js
+++ view/perfilpaci/dist/script.js
@@ -19,5 +19,30 @@ confirm_password.onkeyup = validatemyPassword;
   password.onchange = validatePassword;  
  confirm_password.onkeyup = validatePassword;
 
- 
- 
+ function mask(o, f) {
+  setTimeout(function() {
+    var v = mphone(o.value);
+    if (v != o.value) {
+      o.value = v;
+    }
+  }, 1);
+}
+
+function mphone(v) {
+  var r = v.replace(/\D/g, "");
+  r = r.replace(/^0/, "");
+  if (r.length > 10) {
+    r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+  } else if (r.length > 5) {
+    r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+  } else if (r.length > 2) {
+    r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+  } else {
+    r = r.replace(/^(\d*)/, "($1");
+  }
+  return r;
+}
+document.getElementById('cep').addEventListener('input', function (e) {
+  var x = e.target.value.replace(/\D/g, '').match(/(\d{5})(\d{3}))/);
+  e.target.value =  (x[5] ? '-' + x[3] : '');
+});
