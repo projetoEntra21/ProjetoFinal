b068c85f0809c2712d28f3f227d0211d52b42a50
Lucas bento <bentodeoliveiralucas@gmail.com>
2021-09-28 17:25:29 -0300

    Mudanças nas classes e servlet

--- src/main/java/modelo/dao/paciente/PacienteDAO.java
+++ src/main/java/modelo/dao/paciente/PacienteDAO.java
@@ -1,22 +1,22 @@
-package modelo.dao.paciente;
-
-import java.util.List;
-
-import modelo.entidade.paciente.Paciente;
-
-public interface PacienteDAO {
-
-	public void inserirPaciente(Paciente paciente);
-
-	public void deletarPaciente(Paciente paciente);
-
-	public void atualizarPaciente(Paciente paciente);
-
-	Paciente recuperarPaciente(Paciente paciente);
-
-	Paciente recuperarPacientePeloNome(Paciente paciente);
-
-	Paciente recuperarPacientePeloSobrenome(Paciente paciente);
-
-	List<Paciente> recuperarPacientes();
-}
+//package modelo.dao.paciente;
+//
+//import java.util.List;
+//
+//import modelo.entidade.paciente.Paciente;
+//
+//public interface PacienteDAO {
+//
+//	public void inserirPaciente(Paciente paciente);
+//
+//	public void deletarPaciente(Paciente paciente);
+//
+//	public void atualizarPaciente(Paciente paciente);
+//
+//	Paciente recuperarPaciente(Paciente paciente);
+//
+//	Paciente recuperarPacientePeloNome(Paciente paciente);
+//
+//	Paciente recuperarPacientePeloSobrenome(Paciente paciente);
+//
+//	List<Paciente> recuperarPacientes();
+//}
