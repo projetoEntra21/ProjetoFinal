775bed3182ae42d038a7738b0b9076a8ee4ba14d
juliamohr <87392720+juliamohr@users.noreply.github.com>
2021-10-06 17:14:41 -0300

    cadastro

--- view/medidas/script.js
+++ view/medidas/script.js
@@ -83,10 +83,11 @@ $('.virgula').on('keyup', function() {
       })
 
 
- var cpf = document.querySelector("#cpf");
 
-cpf.addEventListener("keyup", () => {
-  let value = cpf.value.replace(/[^0-9]/g, "").replace(/^([\d]{3})([\d]{3})?([\d]{3})?([\d]{2})?/, "$1.$2.$3-$4");
-  
-  cpf.value = value;
-});
+      var cpf = document.querySelector("#cpf");
+
+      cpf.addEventListener("keyup", () => {
+        let value = cpf.value.replace(/[^0-9]/g, "").replace(/^([\d]{3})([\d]{3})?([\d]{3})?([\d]{2})?/, "$1.$2.$3-$4");
+        
+        cpf.value = value;
+      });
